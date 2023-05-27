3dcc3e0de075e3491d26f48273ad80970ebab220
hayas1 <h4ystack@gmail.com>
2023-05-27 09:16:43 +0000

    modify global scenario

--- test/_global/all.sh
+++ test/_global/all.sh
@@ -6,6 +6,7 @@ source dev-container-features-test-lib
 
 # Definition specific tests
 check "check for tfenv" tfenv --version
+check "check for pyenv" pyenv --version
 
 # Report result
 reportResults
