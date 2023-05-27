3f71acae7ca5e63d5aeca3e001461662821b4bff
hayas1 <h4ystack@gmail.com>
2023-05-27 03:47:32 +0000

    global testing scenario

---
+++ test/_global/all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Optional: Import test library
+source dev-container-features-test-lib
+
+# Definition specific tests
+check "check for tfenv" tfenv --version
+
+# Report result
+reportResults
