4b68b7f6fc26f547602f5b3824b3a7fa472df2c4
hayas1 <h4ystack@gmail.com>
2023-05-27 19:15:56 +0000

    typo

--- test/pyenv/install_without_python.sh
+++ test/pyenv/install_without_python.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "pyenv version" pyenv --version
 check "not install python" test -e "$(which python)"
-check "not install python" test -e "$(which pip)"
+check "not install pip" test -e "$(which pip)"
 
 # Report result
 reportResults
