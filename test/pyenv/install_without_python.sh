6b7da1bafe4dae86e350cef444f4d046aee6954b
hayas1 <h4ystack@gmail.com>
2023-05-28 06:44:58 +0000

    modify test script

--- test/pyenv/install_without_python.sh
+++ test/pyenv/install_without_python.sh
@@ -5,10 +5,18 @@ set -e
 source dev-container-features-test-lib
 export PYENV_ROOT="$HOME/.pyenv" && eval "$(pyenv init -)"
 
+not_exist() {
+    if type "$0" >/dev/null ; then
+        return 1
+    else
+        return 0
+    fi
+}
+
 # Definition specific tests
 check "pyenv version" pyenv --version
-check "not install python" test -e "$(which python)"
-check "not install pip" test -e "$(which pip)"
+check "not install python" not_exist python
+check "not install pip" not_exist pip
 
 # Report result
 reportResults
