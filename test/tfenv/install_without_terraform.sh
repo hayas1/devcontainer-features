bb67d0b1e6d99447ef08c8e82ea691155d1db819
hayas1 <h4ystack@gmail.com>
2023-05-28 06:48:08 +0000

    modify test script

--- test/tfenv/install_without_terraform.sh
+++ test/tfenv/install_without_terraform.sh
@@ -4,9 +4,17 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
+not_installed() {
+    if "$0" --help >/dev/null ; then
+        return 1
+    else
+        return 0
+    fi
+}
+
 # Definition specific tests
 check "tfenv version" tfenv --version
-check "not install terraform" test -e "$(which terraform)"
+check "not install terraform" not_installed terraform
 
 # Report result
 reportResults
