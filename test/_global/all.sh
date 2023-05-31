66dd2b49bd4767f0bd69263db349a816b3cc1ea9
hayas1 <h4ystack@gmail.com>
2023-05-31 14:31:52 +0000

    global test scenario

--- test/_global/all.sh
+++ test/_global/all.sh
@@ -5,6 +5,9 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
+check "check for starship" starship --version
+check "check for sheldon" sheldon --version
+
 check "check for tfenv" tfenv --version
 check "check for pyenv" pyenv --version
 
