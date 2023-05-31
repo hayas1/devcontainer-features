10e1cf2e4650b2f64d5034834185f66eaea90d2b
hayas1 <h4ystack@gmail.com>
2023-05-31 14:53:23 +0000

    path

--- test/starship-sheldon/test.sh
+++ test/starship-sheldon/test.sh
@@ -5,6 +5,7 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
+echo $PATH
 check "check for starship" starship --version
 check "check for sheldon" sheldon --version
 
