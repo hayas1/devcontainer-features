f5a9d906c04bff096ddc97fb558d097a9c07a767
hayas1 <h4ystack@gmail.com>
2023-05-31 21:28:54 +0000

    not not

--- test/starship-sheldon/install_all.sh
+++ test/starship-sheldon/install_all.sh
@@ -7,10 +7,10 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "check for starship" starship --version
 check "check for sheldon" sheldon --version
-check "not install exa" exa --version
-check "not install ripgrep" rg --version
-check "not copy starship.toml" test -e "${HOME}/.config/starship.toml"
-check "not copy sheldon/plugins.toml" test -e "${HOME}/.config/sheldon/plugins.toml"
+check "install exa" exa --version
+check "install ripgrep" rg --version
+check "copy starship.toml" test -e "${HOME}/.config/starship.toml"
+check "copy sheldon/plugins.toml" test -e "${HOME}/.config/sheldon/plugins.toml"
 
 # Report result
 reportResults
