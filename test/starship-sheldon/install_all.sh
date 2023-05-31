8b6de6fe9cb9aac7d0062165eec7a67f8f8a0d5c
hayas1 <h4ystack@gmail.com>
2023-05-31 23:10:09 +0000

    modify wrote .zshrc test

--- test/starship-sheldon/install_all.sh
+++ test/starship-sheldon/install_all.sh
@@ -4,6 +4,10 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
+not() {
+    ! "$@"
+}
+
 # Definition specific tests
 check "check for starship" starship --version
 check "check for sheldon" sheldon --version
@@ -11,6 +15,7 @@ check "install exa" exa --version
 check "install ripgrep" rg --version
 check "copy starship.toml" test -e "${HOME}/.config/starship.toml"
 check "copy sheldon/plugins.toml" test -e "${HOME}/.config/sheldon/plugins.toml"
+check "write .zshrc" not diff "${HOME}/.zshrc" "./zshrc" | cut -c 1 | grep '>'
 
 # Report result
 reportResults
