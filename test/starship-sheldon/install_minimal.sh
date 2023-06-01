f2a9710878a3d902b93e6a26a346e63d73e36f9e
hayas1 <h4ystack@gmail.com>
2023-06-01 11:45:20 +0000

    not

--- test/starship-sheldon/install_minimal.sh
+++ test/starship-sheldon/install_minimal.sh
@@ -5,28 +5,17 @@ set -e
 source dev-container-features-test-lib
 
 tmp=/tmp/devcontainer-feature-starship-sheldon/test
-command_not_exist() {
-    if type "$1" >/dev/null; then
-        return 1
-    else
-        return 0
-    fi
-}
-file_not_exist() {
-    if test -e "$1"; then
-        return 1
-    else
-        return 0
-    fi
+not() {
+    ! "$@"
 }
 
 # Definition specific tests
 check "check for starship" starship --version
 check "check for sheldon" sheldon --version
-check "not install exa" command_not_exist exa
-check "not install ripgrep" command_not_exist rg
-check "not copy starship.toml" file_not_exist "${HOME}/.config/starship.toml"
-check "not copy sheldon/plugins.toml" file_not_exist "${HOME}/.config/sheldon/plugins.toml"
+check "not install exa" not type exa >/dev/null
+check "not install ripgrep" not type rg >/dev/null
+check "not copy starship.toml" not test -e "${HOME}/.config/starship.toml"
+check "not copy sheldon/plugins.toml" not test -e "${HOME}/.config/sheldon/plugins.toml"
 check "write .zshrc" not diff "${HOME}/.zshrc" "${tmp}/zshrc" | cut -c 1 | grep '>'
 
 # Report result
