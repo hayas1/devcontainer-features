208d652f7445e7594db94b739c7548c9b066a0a6
hayas1 <h4ystack@gmail.com>
2023-06-08 14:43:36 +0000

    .devcontainer

---
+++ .devcontainer/entry.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -e
+
+# change home directory owner from root
+echo "change home directory ${HOME} owner to ${USER}..."
+sudo chown -R "${USER}:${USER}" "${HOME}"
+echo "done"
+
+# for develop with python
+if [ -e "./requirements.txt" ]; then
+    pip install -r "./requirements.txt"
+fi
+
+# for develop with go
+if type go >/dev/null 2>&1; then
+    go install -v golang.org/x/tools/gopls@latest
+fi
---
+++ .devcontainer/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+starship --version
+sheldon --version
+
+python --version
+go --version
+cargo --version
+node --version
+
+docker --version
+gcloud --version
+kubectl version --client
+helm version --client
