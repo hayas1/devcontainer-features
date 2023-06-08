4b7026d51ad769d4b011d1e8edd4f0e99cc2a11a
hayas1 <h4ystack@gmail.com>
2023-06-08 14:49:08 +0000

    project settings

--- .devcontainer/entry.sh
+++ .devcontainer/entry.sh
@@ -14,3 +14,7 @@ fi
 if type go >/dev/null 2>&1; then
     go install -v golang.org/x/tools/gopls@latest
 fi
+
+# project settings
+npm install -g @devcontainers/cli
+gh extension install https://github.com/nektos/gh-act
