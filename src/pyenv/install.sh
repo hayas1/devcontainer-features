3716d2bde59dd434cd07e6b5e7e28918e97dad8c
hayas1 <h4ystack@gmail.com>
2023-06-09 14:38:55 +0000

    -y

--- src/pyenv/install.sh
+++ src/pyenv/install.sh
@@ -12,7 +12,7 @@ bin=/usr/local/bin
 # dependencies
 apt-get update -y && apt-get install -y build-essential zlib1g-dev libssl-dev libsqlite3-dev \
     libbz2-dev libncurses5-dev libgdbm-dev liblzma-dev tcl-dev tk-dev libreadline-dev libffi-dev &&
-    apt-get install git && # ??? https://qiita.com/yagince/items/deba267f789604643bab
+    apt-get install -y git && # ??? https://qiita.com/yagince/items/deba267f789604643bab
     apt-get clean && rm -rf /var/lib/apt/lists
 # https://github.com/pyenv/pyenv#basic-github-checkout
 git clone https://github.com/pyenv/pyenv.git "$lib" --branch "${VERSION}" --depth 1
