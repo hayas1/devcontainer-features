82ab8e7cb3d09e37621a3af20946f75e6e314546
hayas1 <h4ystack@gmail.com>
2023-06-11 02:05:39 +0000

    update options

--- src/zsh-dotfiles/install.sh
+++ src/zsh-dotfiles/install.sh
@@ -1,34 +1,63 @@
 #!/bin/sh -e
-WITH_STARSHIP_CONFIG=${WITH_STARSHIP_CONFIG:-"true"}
-WITH_SHELDON_CONFIG=${WITH_SHELDON_CONFIG:-"true"}
-WITH_SOME_TOOLS=${WITH_SOME_TOOLS:-"true"}
-OVERWRITE_ZSHRC=${OVERWRITE_ZSHRC:-"true"}
+PLUGIN_MANAGER=${PLUGIN_MANAGER:-"without"}
+THEME=${THEME:-"without"}
+WITH_PLUGINS=${WITH_PLUGINS:-"false"}
+WITH_TOOLS=${WITH_TOOLS:-"false"}
+WITH_ALIASES=${WITH_ALIASES:-"false"}
 
 # for test
 tmp=/tmp/devcontainer-feature-zsh-dotfiles/test
 mkdir -p "$tmp" && cp -r . "$tmp"
 
-# starship
-if [ "$WITH_STARSHIP_CONFIG" = "true" ]; then
+if [ "$PLUGIN_MANAGER" = "sheldon" ]; then
+    # overwrite .zshrc
+    cat ./home/zshrc >"${_REMOTE_USER_HOME}/.zshrc"
+fi
+
+if [ "$WITH_PLUGINS" = "true" ]; then
+    case "$PLUGIN_MANAGER" in
+    'without')
+        echo 'for install plugins, some kind of plugin manager must be available'
+        exit 1
+        ;;
+    'oh-my-zsh')
+        ./plugins-omz.sh
+        ;;
+    'sheldon')
+        mkdir -p "${_REMOTE_USER_HOME}/.config/sheldon/"
+        cp ./home/config/sheldon/plugins.toml "${_REMOTE_USER_HOME}/.config/sheldon/plugins.toml"
+        ;;
+    *)
+        echo "unrecognized plugin manager: ${PLUGIN_MANAGER}"
+        exit 1
+        ;;
+    esac
+fi
+
+case "$THEME" in
+'without') ;;
+'powerlevel10k')
+    cp ./home/p10k.zsh "${_REMOTE_USER_HOME}/.p10k.zsh"
+    ;;
+'starship')
     mkdir -p "${_REMOTE_USER_HOME}/.config/"
     cp ./home/config/starship.toml "${_REMOTE_USER_HOME}/.config/starship.toml"
-fi
+    ;;
+*)
+    echo "unrecognized theme: ${THEME}"
+    exit 1
+    ;;
+esac
 
-# sheldon
-if [ "$WITH_SHELDON_CONFIG" = "true" ]; then
-    mkdir -p "${_REMOTE_USER_HOME}/.config/sheldon/"
-    cp ./home/config/sheldon/plugins.toml "${_REMOTE_USER_HOME}/.config/sheldon/plugins.toml"
-fi
-
-# tools
-if [ "$WITH_SOME_TOOLS" = "true" ]; then
+if [ "$WITH_TOOLS" = "true" ]; then
     apt-get update -y && apt-get upgrade -y &&
         apt-get install -y zsh wget git procps htop connect-proxy sudo \
             exa ripgrep fd-find bat hexyl &&
         apt-get clean && rm -rf /var/lib/apt/lists
 fi
 
-# overwrite zshrc
-if [ "$OVERWRITE_ZSHRC" = "true" ]; then
-    cat ./home/zshrc >"${_REMOTE_USER_HOME}/.zshrc"
+if [ "$WITH_ALIASES" = "true" ]; then
+    echo >>"$_REMOTE_USER_HOME/.zshrc"
+    cp ./home/aliases "${_REMOTE_USER_HOME}/.aliases"
+    cat ./aliases.zshrc >>"$_REMOTE_USER_HOME/.zshrc"
 fi
