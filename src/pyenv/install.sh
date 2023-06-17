fed2829ec07fcf9c934595b890ed3702a1fa75ae
hayas1 <h4ystack@gmail.com>
2023-06-17 02:41:27 +0000

    add version `latest`

--- src/pyenv/install.sh
+++ src/pyenv/install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -e
-VERSION=${VERSION:-"master"}
+VERSION=${VERSION:-"latest"}
 WITH=${WITH:-"none"}
 RC_FILE=${RC_FILE:-".zshrc"}
 
@@ -7,19 +7,32 @@ RC_FILE=${RC_FILE:-".zshrc"}
 tmp=/tmp/devcontainer-feature-pyenv/test
 mkdir -p "$tmp" && cp -r . "$tmp"
 
+# install path
 export DEBIAN_FRONTEND=noninteractive
 lib="${_REMOTE_USER_HOME}/.pyenv"
 bin=/usr/local/bin
+
 # dependencies
-apt-get update -y && apt-get install -y git build-essential zlib1g-dev libssl-dev libsqlite3-dev \
+apt-get update -y && apt-get install -y curl git build-essential zlib1g-dev libssl-dev libsqlite3-dev \
     libbz2-dev libncurses5-dev libgdbm-dev liblzma-dev tcl-dev tk-dev libreadline-dev libffi-dev &&
     apt-get clean && rm -rf /var/lib/apt/lists
-# https://github.com/pyenv/pyenv#basic-github-checkout
-git clone https://github.com/pyenv/pyenv.git "$lib" --branch "${VERSION}" --depth 1
+
+# find latest
+repository='https://github.com/pyenv/pyenv'
+if [ "$VERSION" = "latest" ]; then
+    VERSION=$(
+        curl -fsL -o /dev/null -w %{url_effective} "${repository}/releases/latest" |
+            sed -r "s,^${repository}/releases/tag/(.*)$,\1,g"
+    )
+fi
+
+# install pyenv https://github.com/pyenv/pyenv#basic-github-checkout
+git clone "${repository}" "$lib" --branch "${VERSION}" --depth 1
 pushd "$lib" && src/configure && make -C src
 popd
 ln -s "$lib/bin/pyenv" "$bin/pyenv"
 
+# install python also ?
 if [ "$WITH" != "none" ]; then
     if [ "$WITH" = "latest" ]; then
         WITH="3:latest"
@@ -32,5 +45,6 @@ if [ "$WITH" != "none" ]; then
     pyenv global "$GLOBAL"
 fi
 
-printf '\n' >>"${_REMOTE_USER_HOME}/${RC_FILE}"
+# run command
+echo >>"${_REMOTE_USER_HOME}/${RC_FILE}"
 cat "./rc" >>"${_REMOTE_USER_HOME}/${RC_FILE}"
