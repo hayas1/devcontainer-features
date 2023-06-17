d3a0a32e9b2bf01c1e639b370f4469a29166373d
hayas1 <h4ystack@gmail.com>
2023-06-17 03:42:17 +0000

    update version

--- test/gcloud-cli/install_with_version.sh
+++ test/gcloud-cli/install_with_version.sh
@@ -10,7 +10,7 @@ not() {
 
 # Definition specific tests
 check "check for gcloud" gcloud --version | grep 'Google Cloud SDK 433.0.1'
-check "check for kubectl" kubectl version --client --output=yaml | grep 'gitVersion: v1.27.2'
+check "check for kubectl" kubectl version --client --output=yaml | grep 'gitVersion: v1.27.3'
 check "check for helm" helm version --client | grep 'Version:"v3.12.0"'
 check "check for gcloud completion" diff "${HOME}/.zshrc" "${tmp}/zshrc/gcloud.zshrc" | cut -c 1 | not grep '>'
 check "check for kubectl completion" diff "${HOME}/.zshrc" "${tmp}/zshrc/kubectl.zshrc" | cut -c 1 | not grep '>'
