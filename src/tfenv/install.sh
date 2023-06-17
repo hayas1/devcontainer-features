df9f2764b167f2b3e50635d28db5d6fb47b6b24f
hayas1 <h4ystack@gmail.com>
2023-06-17 02:41:30 +0000

    add version `latest`

--- src/tfenv/install.sh
+++ src/tfenv/install.sh
@@ -1,16 +1,30 @@
 #! /bin/bash -e
-VERSION=${VERSION:-"master"}
+VERSION=${VERSION:-"latest"}
 WITH=${WITH:-"none"}
 
+# install path
 lib=/usr/lib/tfenv
 bin=/usr/local/bin
-# https://github.com/tfutils/tfenv#manual
-apt-get update -y && apt-get install -y zip git &&
+
+# dependencies
+apt-get update -y && apt-get install -y curl zip git &&
     apt-get clean && rm -rf /var/lib/apt/lists
-git clone https://github.com/tfutils/tfenv.git "$lib" --branch "${VERSION}" --depth 1
+
+# find latest
+repository='https://github.com/tfutils/tfenv'
+if [ "$VERSION" = "latest" ]; then
+    VERSION=$(
+        curl -fsL -o /dev/null -w %{url_effective} "${repository}/releases/latest" |
+            sed -r "s,^${repository}/releases/tag/(.*)$,\1,g"
+    )
+fi
+
+# install tfenv https://github.com/tfutils/tfenv#manual
+git clone "${repository}" "$lib" --branch "${VERSION}" --depth 1
 ln -s "$lib/bin/tfenv" "$bin/tfenv"
 ln -s "$lib/bin/terraform" "$bin/terraform"
 
+# install terraform also ?
 if [ "$WITH" != "none" ]; then
     tfenv install "$WITH"
     tfenv use "$WITH"
