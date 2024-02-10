de5b468e9ffde325ebe20e75e91d9a0833aefa5c
hayas1 <h4ystack@gmail.com>
2024-02-10 05:01:49 +0000

    modify test

--- test/powerlevel10k/install_with_sheldon.sh
+++ test/powerlevel10k/install_with_sheldon.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "check for sheldon" sheldon --version
 check "check sheldon toml" grep "\[plugins.powerlevel10k\]" "${HOME}/.config/sheldon/plugins.toml"
-check "check sheldon toml" grep "github = 'romkatv/powerlevel10k'" "${HOME}/.config/sheldon/plugins.toml"
+check "check sheldon toml" grep 'github = "romkatv/powerlevel10k"' "${HOME}/.config/sheldon/plugins.toml"
 
 # Report result
 reportResults
