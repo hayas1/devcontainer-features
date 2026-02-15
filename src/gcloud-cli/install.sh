e8aaf84f533838389980bf6995f430bb72bc995d
hayas1 <h4ystack@gmail.com>
2026-02-15 16:02:21 +0000

    modify install gcloud-cli

--- src/gcloud-cli/install.sh
+++ src/gcloud-cli/install.sh
@@ -17,10 +17,10 @@ apt-get clean && rm -rf /var/lib/apt/lists
 printf '\n' >>"${_REMOTE_USER_HOME}/.${COMPLETION}rc"
 
 # install gcloud https://cloud.google.com/sdk/docs/install?hl=ja#deb
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" |
-    tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg |
-    apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+    gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" |
+    tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 if [ "$VERSION" = "latest" ]; then
     apt-get update -y && apt-get install -y google-cloud-sdk
 else
